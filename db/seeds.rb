20aff4a6bc933533a274e7489158f094abd9f604
marcoaam <marco.army@gmail.com>
2014-08-20 18:09:25 +0100

    seeds pending

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy.all
+
+Post.create(title: 'Post number', image: Rails.root.join('spec/images/surf2.jpg'), tags_list: '#great #wave')
