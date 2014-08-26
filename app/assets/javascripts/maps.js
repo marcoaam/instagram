ae5f80509401f761c012921787dd1024e4d13ee2
marcoaam <marco.army@gmail.com>
2014-08-26 10:41:57 +0100

    JS fixed

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-	$.get('/posts/' + $('#map').data("id"), function(post) {
-		var map = new GMaps({
-		  div: '#map',
-		  lat: -12.043333,
-		  lng: -77.028333
+	var postId = $('#map').data('id');
+	if(postId) {
+		$.get('/posts/' + $('#map').data("id"), function(post) {
+			var map = new GMaps({
+			  div: '#map',
+			  lat: -12.043333,
+			  lng: -77.028333
+			});
+			GMaps.geocode({
+			  address: post.address,
+			  callback: function(results, status) {
+			    if (status == 'OK') {
+			      var latlng = results[0].geometry.location;
+			      map.setCenter(latlng.lat(), latlng.lng());
+			      map.addMarker({
+			        lat: latlng.lat(),
+			        lng: latlng.lng()
+			      });
+			    }
+			  }
+			});
 		});
-		GMaps.geocode({
-		  address: post.address,
-		  callback: function(results, status) {
-		    if (status == 'OK') {
-		      var latlng = results[0].geometry.location;
-		      map.setCenter(latlng.lat(), latlng.lng());
-		      map.addMarker({
-		        lat: latlng.lat(),
-		        lng: latlng.lng()
-		      });
-		    }
-		  }
-		});
-	});
+	}
 });
