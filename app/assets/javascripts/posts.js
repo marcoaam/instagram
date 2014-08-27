8d1bdae04e8d7fc706c87d294fd167fde70241eb
marcoaam <marco.army@gmail.com>
2014-08-27 18:36:32 +0100

    websockets in progress

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,13 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+	Pusher.log = function(message) {
+	  if (window.console && window.console.log) {
+	    window.console.log(message);
+	  }
+	};
+
+	var pusher = new Pusher('b3a141975cd83e2d9ace');
+	var channel = pusher.subscribe('instagram_test');
+	channel.bind('my_event', function(data) {
+	  $('body').prepend('<img src=' + data.message + '></img>');
+	});
+});
