737f23696b60b88b80a34b24c61cad46a0f02ceb
marcoaam <marco.army@gmail.com>
2014-08-28 17:58:52 +0100

    button added

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	var pusher = new Pusher('b3a141975cd83e2d9ace');
 	var channel = pusher.subscribe('instagram_test');
 	channel.bind('my_event', function(data) {
-	  $('.container .row').prepend('<div class="col-md-12"><a href="/" class="new_post">' + data.message + '</a></div>');
+	  $('.container .row').prepend('<div class="col-md-12"><div class="new_post"><a href="/" class="new_post_link">' + data.message + '</a></div></div>');
 	});
 });
